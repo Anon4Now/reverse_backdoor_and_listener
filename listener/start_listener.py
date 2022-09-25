502d08b8890bb418159203a3ff1ec8c0b61c600b
Anon4Now <tutkoda@gmail.com>
2022-09-25 10:26:25 -0400

    moved the socket instantiation to this file

--- listener/start_listener.py
+++ listener/start_listener.py
@@ -1,7 +1,13 @@
-#!/usr/env/bin python
+"""Module containing the main func to run the listener"""
 
+# Standard Library imports
+import socket
+
+# Local App imports
 from listener import Listener
 
+listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 if __name__ == '__main__':
-    listener = Listener("192.168.229.130", 4444)
-    listener.startListener()
+    listener = Listener(local_ip="192.168.229.130", local_port=4444, listener=listener)
+    listener.start_listener()
