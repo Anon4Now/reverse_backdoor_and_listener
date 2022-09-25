d21f2a0abf11eb325e723ff2e637b040c0ba49be
Anon4Now <tutkoda@gmail.com>
2022-09-25 11:05:09 -0400

    updated Listener class call

--- listener/start_listener.py
+++ listener/start_listener.py
@@ -6,8 +6,7 @@ import socket
 # Local App imports
 from listener import Listener
 
-listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 if __name__ == '__main__':
-    listener = Listener(local_ip="192.168.229.130", local_port=4444, listener=listener)
+    listener = Listener(local_ip="192.168.229.130", local_port=4444, listener=socket.socket(socket.AF_INET, socket.SOCK_STREAM))
     listener.start_listener()
